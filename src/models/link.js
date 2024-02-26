4fb01e91439aa2aa119731f6dbfa946a58938ab6
Your Name <you@example.com>
2024-02-26 09:31:26 +0100

    Create the model layer, made of links and entities

---
+++ src/models/link.js
@@ -0,0 +1,10 @@
+class Link {
+  constructor(url, depth = 0) {
+    this.url = url;
+    this.depth = depth;
+  }
+
+  // ...
+}
+
+module.exports = Link;
---
+++ src/models/scrape.js
@@ -0,0 +1,11 @@
+class Scrape {
+  constructor(url, data, depth = 0) {
+    this.url = url;
+    this.data = data;
+    this.depth = depth;
+  }
+
+  // ...
+}
+
+module.exports = Scrape;
